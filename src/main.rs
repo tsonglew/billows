622b1adacb93a9883ab44423f4d996877745eb20
Tsonglew <tsonglew@gmail.com>
2024-10-03 22:59:08 +0800

    wip: use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+    }
 }
