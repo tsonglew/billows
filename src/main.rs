d5507b3026809dcaa0fc1b80e1a668d95aad2f97
Tsonglew <tsonglew@gmail.com>
2024-10-04 23:02:45 +0800

    wip: update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::error::Error;
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
     }
+}
+
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
 }
