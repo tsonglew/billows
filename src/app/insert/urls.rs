46529b030c75bd8476de68afc34c1fcf148e7cf4
Tsonglew <tsonglew@gmail.com>
2024-10-07 23:51:15 +0800

    fix: insert view use path

--- src/app/insert/urls.rs
+++ src/app/insert/urls.rs
@@ -1,8 +1,7 @@
 use actix_web::{web, HttpResponse};
 
+use super::views;
+
 pub fn register_urls(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(web::scope("/insert").route(
-        "/reservation",
-        web::post().to(|| async { HttpResponse::Ok() }),
-    ));
+    cfg.service(web::scope("/insert").route("/reservation", web::post().to(views::search_view)));
 }
--- src/app/insert/views.rs
+++ src/app/insert/views.rs
@@ -1,1 +1,5 @@
-mod errors;
+use actix_web::{HttpResponse, HttpResponseBuilder};
+
+pub async fn search_view() -> HttpResponse {
+    return HttpResponse::Ok().body("hello insert!");
+}
